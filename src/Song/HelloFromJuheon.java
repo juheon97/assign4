b90924478ad7801bfbdb59a6502d2164d4d68611
Joe <Pretzelman718@gmail.com>
2019-05-17 19:08:44 -0600

    Joe also says hello

--- src/Song/HelloFromJuheon.java
+++ src/Song/HelloFromJuheon.java
@@ -3,6 +3,7 @@ package Song;
 public class HelloFromJuheon {
 
     public static void main(String[] args) {
-	System.out.print("Hello, My name is Juheon Song.");
+        System.out.print("Hello, My name is Juheon Song.");
+        System.out.print("Hello Juheon Song, my name is Joe Cheney.");
     }
 }
